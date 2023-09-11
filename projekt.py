ef5150646392d7bfc3e5bffe7bf1c6fcbfa168e0
Maciej1302 <roziomaciej@gmail.com>
2023-09-12 01:15:20 +0200

    123

--- projekt.py
+++ projekt.py
@@ -1,7 +1,7 @@
 
 #print("Wpisz swoje składniki odzielone spacją: ")
-lista_skladnikow_uzytkownika=tuple()
-lista_skladnikow_uzytkownika=input().split()
+#lista_skladnikow_uzytkownika=tuple()
+#lista_skladnikow_uzytkownika=input().split()
 
 file=open('skladniki.txt','r')
 
@@ -9,9 +9,8 @@ f=file.readlines()
 
 for line in f:
     skladniki_przepisu=tuple(line.split())
-    if skladniki_przepisu==lista_skladnikow_uzytkownika:
-        print(skladniki_przepisu)
-    #print(skladniki_przepisu)
+    print(skladniki_przepisu)
+    
 file.close()
 
 """123123"""
