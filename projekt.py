e881044fb6d32506c88e1df74ee6b7fcb7f63c7a
Maciej1302 <roziomaciej@gmail.com>
2023-09-11 16:23:12 +0200

    pliki

--- projekt.py
+++ projekt.py
@@ -1,1 +1,15 @@
-print("Wpisz składniki ")
+
+#print("Wpisz swoje składniki odzielone spacją: ")
+lista_skladnikow_uzytkownika=tuple()
+lista_skladnikow_uzytkownika=input().split()
+
+file=open("skladniki.txt","r")
+
+f=file.readlines()
+
+for line in f:
+    skladniki_przepisu=tuple(line.split())
+
+file.close()
+
+print(skladniki_przepisu)
