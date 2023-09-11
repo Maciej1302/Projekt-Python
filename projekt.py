4df0e229c3e138e3daf9d5e8bc4ae691c3a70048
Maciej1302 <roziomaciej@gmail.com>
2023-09-12 01:44:07 +0200

    Pliki dzialaja

--- projekt.py
+++ projekt.py
@@ -3,17 +3,14 @@
 #lista_skladnikow_uzytkownika=tuple()
 #lista_skladnikow_uzytkownika=input().split()
 
-file=open("plik.txt",'r')
+file=open("skladniki.txt",'r')
 
-f=file.readline()
+f=file.readlines()
 
-print(f)
-""""
+
 for line in f:
     skladniki_przepisu=tuple(line.split())
-    if skladniki_przepisu==lista_skladnikow_uzytkownika:
-        print(skladniki_przepisu)
-    #print(skladniki_przepisu)
+    print(skladniki_przepisu)
 file.close()
 
 """123123"""
