b7c766e8fcf2fabbfb3db1af99348b4848f81981
Dominik <frexorplay@gmail.com>
2023-09-12 01:35:34 +0200

    idk_czy_dziala

--- projekt.py
+++ projekt.py
@@ -11,7 +11,7 @@ for line in f:
     skladniki_przepisu=tuple(line.split())
     if skladniki_przepisu==lista_skladnikow_uzytkownika:
         print(skladniki_przepisu)
-    #print(skladniki_przepisu)
+
 file.close()
 
 """123123"""
