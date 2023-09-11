c98720bd593e9b44f3c82714da1f6d0bf6ec4a66
Maciej1302 <roziomaciej@gmail.com>
2023-09-12 01:28:37 +0200

    jebel

--- projekt.py
+++ projekt.py
@@ -3,14 +3,16 @@
 #lista_skladnikow_uzytkownika=tuple()
 #lista_skladnikow_uzytkownika=input().split()
 
-file=open('skladniki.txt','r')
+file=open("plik.txt",'r')
 
-f=file.readlines()
+f=file.readline()
 
+print(f)
+""""
 for line in f:
     skladniki_przepisu=tuple(line.split())
     print(skladniki_przepisu)
-    
+    """
 file.close()
 
 """123123"""
