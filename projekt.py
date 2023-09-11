93d6fa95f0b89a793a986ef26468ec389aad266f
Maciej1302 <roziomaciej@gmail.com>
2023-09-11 16:31:15 +0200

    plikczki

--- projekt.py
+++ projekt.py
@@ -1,15 +1,15 @@
 
 #print("Wpisz swoje składniki odzielone spacją: ")
 lista_skladnikow_uzytkownika=tuple()
-lista_skladnikow_uzytkownika=input().split()
+#lista_skladnikow_uzytkownika=input().split()
 
-file=open("skladniki.txt","r")
+file=open('skladniki.txt','r')
 
 f=file.readlines()
 
 for line in f:
     skladniki_przepisu=tuple(line.split())
-
+    print(skladniki_przepisu)
 file.close()
 
-print(skladniki_przepisu)
+
