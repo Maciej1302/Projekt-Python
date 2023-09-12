f6e14f1f259a471bad9998ec387e193ef4be9672
Maciej1302 <roziomaciej@gmail.com>
2023-09-12 16:41:05 +0200

    Dzialajaca porwownywarka skladnikow zwracajaca numery przepisow

--- projekt.py
+++ projekt.py
@@ -1,14 +1,28 @@
 
 #print("Wpisz swoje składniki odzielone spacją: ")
-#lista_skladnikow_uzytkownika=tuple()
+lista_skladnikow_uzytkownika=["szynka","pomidor","kawior"]
 #lista_skladnikow_uzytkownika=input().split()
 
-file=open("skladniki.txt",'r')
+def porownywarka_skladnikow_uzytkownika_z_przepisem():
 
-f=file.readlines()
+    lista_z_numerami_przepisow=[]
+    file=open("skladniki.txt",'r')
+    f=file.readlines()
 
+    for line in f:
+        ilosc_pasujacych_skladnikow=0
+        skladniki_przepisu=line.split()
+        ilosc=len(skladniki_przepisu)
+        for x in lista_skladnikow_uzytkownika:
+            if x in skladniki_przepisu:
+                ilosc_pasujacych_skladnikow+=1
 
-for line in f:
-    skladniki_przepisu=tuple(line.split())
-    print(skladniki_przepisu)
-file.close()
+        if ilosc_pasujacych_skladnikow==ilosc-1:
+            print(skladniki_przepisu)
+            lista_z_numerami_przepisow.append(skladniki_przepisu[0])
+        skladniki_przepisu.clear()
+        
+    file.close()
+    return lista_z_numerami_przepisow
+
+print(porownywarka_skladnikow_uzytkownika_z_przepisem())
