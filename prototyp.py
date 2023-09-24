c1e4c8c1b0fa8f25e8a83e62a2f6cf903988ff80
Maciej1302 <roziomaciej@gmail.com>
2023-09-24 02:10:14 +0200

    users

--- prototyp.py
+++ prototyp.py
@@ -6,13 +6,43 @@ from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
-from projekt import szukarka
-class FirstWindow(Screen):
+from projekt import szukarka 
+from projekt import check_user_existence 
+from projekt import add_user
+
+
+class MyPopup(Popup):
+
     pass
 
-#komentarz
-class SecondWindow(Screen):
+
+
+class FirstWindow(Screen):
+    
     pass
+
+
+def open_pop_up(self):
+    pops = MyPopup()
+    pops.open()
+
+
+
+
+class SecondWindow(Screen):
+         
+            
+
+         def verification(self):
+            login = self.ids.login.text
+            password = self.ids.password.text
+            if check_user_existence(login):
+                App.get_running_app().root.current='third'
+                App.get_running_app().root.transition.direction = "up"
+            else:
+                App.get_running_app().root.current='second'
+                open_pop_up(self)
+                
 
 
 class ThirdWindow(Screen):
@@ -24,6 +54,24 @@ class ThirdWindow(Screen):
             final_recipe=final_recipe+item+"\n"+"\n"
         
         self.ids.recipe.text=final_recipe
+        
+    pass
+
+class RegisterWindow(Screen):
+    def verification(self):
+        name=self.ids.name.text
+        surname=self.ids.surname.text
+        passw=self.ids.register_passw.text
+        email= self.ids.email.text
+        if check_user_existence(email):
+            App.get_running_app().root.current='second'
+        
+        elif name==""or surname==""or passw==""or email=="":
+            open_pop_up(self)
+        else:
+            add_user(email,passw,name,surname)
+            App.get_running_app().root.current='second'
+         
     pass
 
 class WindowManager(ScreenManager):
