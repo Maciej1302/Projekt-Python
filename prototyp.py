2b766af191c494318b304334f4de309cd6cbaaa2
Dominik <106604755+dzebrowskii@users.noreply.github.com>
2023-09-24 22:50:46 +0200

    Add files via upload

--- prototyp.py
+++ prototyp.py
@@ -6,73 +6,39 @@ from kivy.properties import ObjectProperty
 from kivy.uix.label import Label
 from kivy.uix.floatlayout import FloatLayout
 from kivy.uix.popup import Popup
-from projekt import szukarka 
-from projekt import check_user_existence 
-from projekt import add_user
-
-
-class MyPopup(Popup):
-
-    pass
-
-
-
+from projekt import szukarka
 class FirstWindow(Screen):
-    
     pass
 
-
-def open_pop_up(self):
-    pops = MyPopup()
-    pops.open()
 
 
 
 
 class SecondWindow(Screen):
-         
-            
+    pass
 
-         def verification(self):
-            login = self.ids.login.text
-            password = self.ids.password.text
-            if check_user_existence(login):
-                App.get_running_app().root.current='third'
-                App.get_running_app().root.transition.direction = "up"
-            else:
-                App.get_running_app().root.current='second'
-                open_pop_up(self)
-                
+class GuestApp(Screen,Widget):
+    def GuestPress(self):
+        ingredients= self.ids.guest_ingredients.text
+        self.ids.guest_space.text=getRecipe(ingredients)
+    pass
 
-
-class ThirdWindow(Screen):
+class ThirdWindow(Screen,Widget):
     def press(self):
         ingredients= self.ids.ingredients.text
-        recipe=szukarka(ingredients)
-        final_recipe=""
-        for item in recipe:
-            final_recipe=final_recipe+item+"\n"+"\n"
-        
-        self.ids.recipe.text=final_recipe
-        
+        self.ids.recipe.text=getRecipe(ingredients)
     pass
 
-class RegisterWindow(Screen):
-    def verification(self):
-        name=self.ids.name.text
-        surname=self.ids.surname.text
-        passw=self.ids.register_passw.text
-        email= self.ids.email.text
-        if check_user_existence(email):
-            App.get_running_app().root.current='second'
-        
-        elif name==""or surname==""or passw==""or email=="":
-            open_pop_up(self)
-        else:
-            add_user(email,passw,name,surname)
-            App.get_running_app().root.current='second'
-         
-    pass
+
+def getRecipe(id):
+    recipe=szukarka(id)
+    final_recipe=""
+    for item in recipe:
+        final_recipe=final_recipe+item+"\n"+"\n"
+    return final_recipe
+
+
+
 
 class WindowManager(ScreenManager):
     pass
@@ -92,6 +58,3 @@ class MyMainApp(App):
 
 if __name__ == "__main__":
     MyMainApp().run()
-
-#123
-#321
